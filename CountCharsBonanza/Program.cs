5d9a2ff13c9559cff6127cae67524cf4cf595476
GET Academy <terje@getacademy.no>
2022-05-16 12:43:56 +0200

    Initial commit

---
+++ CountCharsBonanza/Program.cs
@@ -0,0 +1,41 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        //Demo1();
+
+    }
+
+    public static void Demo1()
+    {
+        var counts = new int[250];
+
+        string text = "something";
+        while (!string.IsNullOrWhiteSpace(text))
+        {
+            text = Console.ReadLine();
+            CountChars(text, counts);
+            ShowStats(counts);
+        }
+    }
+
+    private static void CountChars(string? text, int[] counts)
+    {
+        foreach (var character in text ?? string.Empty)
+        {
+            counts[(int)character]++;
+        }
+    }
+
+    private static void ShowStats(int[] counts)
+    {
+        for (var i = 0; i < counts.Length; i++)
+        {
+            if (counts[i] > 0)
+            {
+                var character = (char)i;
+                Console.WriteLine(character + " - " + counts[i]);
+            }
+        }
+    }
+}
